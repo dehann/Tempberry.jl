645f567bcaaf4d9e580c6cd71f3cecb707db03bb
dehann <dehann.fourie@gmail.com>
2017-11-26 12:23:11 -0500

    fix lstherm

--- src/Tempberry.jl
+++ src/Tempberry.jl
@@ -12,7 +12,17 @@ export
 List thermometers currently connected and discoverable through `/sys/bus/w1/devices/28-*`
 """
 function lstherm()
-  run(`ls /sys/bus/w1/devices/28-*`)
+  here=pwd()
+  cd("/sys/bus/w1/devices/")
+  dirfiles = readdir()
+  keeplist = String[]
+  for fin in dirfiles
+    if fin[1:3] = "28-"
+      push!(keeplist, fin)
+    end
+  end
+  cd(here)
+  keeplist
 end
 
 
