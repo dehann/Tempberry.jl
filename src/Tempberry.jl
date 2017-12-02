06c3fc22f2f28e9ef9252dae5e7c8596171aa09d
Kristen Hunter-Cevera <Kristen@bluemorpho.hsd1.ma.comcast.net>
2017-12-02 13:46:22 -0500

    added a table

--- src/Tempberry.jl
+++ src/Tempberry.jl
@@ -40,23 +40,13 @@ function readtherm(thermname; path="/sys/bus/w1/devices")
   parse(Float64, l2s[2])/1000.0
 end
 
-#function gentempspage()
-#  htmlstr = ""
-#
-#  return htmlstr
-#end
+
+function getmax(arr)
+  return maximum(arr)
+end
 
 
-function defaultpage(req, res)
-    if ismatch(r"^/hello/",req.resource)
-        string("Hello ", split(req.resource,'/')[3], "!")
-    else
-        404
-    end
-  end
-
-
-function hostwebpage(;port=8000)
+function hostwebpage(;port=9000)
   http = HttpHandler() do req::Request, res::Response
 
       Response(  defaultpage(req, res)  )
@@ -67,26 +57,61 @@ function hostwebpage(;port=8000)
   nothing
 end
 
-function gentemppage(req, res, temp1, temp2)
-    if ismatch(r"^/hello/", req.resource)
-        return "temp1=$(temp1), temp2=$(temp2)!"
-    else
-        return 404
-    end
-end
-
-function hosttempswebpage(;port=8000)
+function hosttempswebpage(;port=9000)
   @show therms = lstherm()
 
   http = HttpHandler() do req::Request, res::Response
-      temp1 = readtherm(therms[1])
-      temp2 = readtherm(therms[2])
-      Response(  gentemppage(req, res, temp1, temp2)  )
+      # temp1 = readtherm(therms[1])
+      # temp2 = readtherm(therms[2])
+      Response(  gentemppage(req, res, therms)  )
   end
   server = Server( http )
   run(server, port=port)
   nothing
 end
+
+function maketemptable(temp1, temp2)
+  textblock = """
+  <table style=\"width:25%\" border=\"1\">
+   <tr>
+     <th>Temp1</th>
+     <th>Temp2</th>
+   </tr>
+   <tr>
+     <td>$(temp1)</td>
+     <td>$(temp2)</td>
+   </tr>
+  </table>
+
+  <a href=\"file:///tmpdata/mydatas.csv\">GetData</a>
+  """
+end
+
+function maketempspage(therms)
+  temp1 = readtherm(therms[1])
+  temp2 = readtherm(therms[2])
+  maketemptable(temp1, temp2)
+end
+
+
+function gentemppage(req, res, thermslocal)
+    if ismatch(r"^/", req.resource)
+        # return "temp1=$(temp1), temp2=$(temp2)!"
+        return maketemptable(thermslocal)
+    else
+        return 404
+    end
+end
+
+function defaultpage(req, res)
+    if ismatch(r"^/",req.resource)
+        # string("Hello ", split(req.resource,'/')[3], "!")
+        maketemptable(1,2)
+    else
+        @show "unknown request $(req.resource)"
+        404
+    end
+  end
 
 
 end
